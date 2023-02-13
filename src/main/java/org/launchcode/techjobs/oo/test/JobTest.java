79d2bef232043b8393055f264bbe6dbee213e43f
DH001 <dhutchen01@gmail.com>
2023-02-13 16:18:38 -0600

    p4 done

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,39 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId() {
+         Job testJobOne = new Job();
+         Job testJobTwo = new Job();
+         assertNotEquals(testJobTwo, testJobOne);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job testJobThree = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJobThree.getName() != null);
+        assertTrue(testJobThree.getEmployer() instanceof Employer);
+        assertTrue(testJobThree.getLocation() instanceof Location);
+        assertTrue(testJobThree.getPositionType() instanceof PositionType);
+        assertTrue(testJobThree.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Product tester", testJobThree.getName());
+        assertEquals("ACME", testJobThree.getEmployer().toString());
+        assertEquals("Desert", testJobThree.getLocation().toString());
+        assertEquals("Quality control", testJobThree.getPositionType().toString());
+        assertEquals("Persistence", testJobThree.getCoreCompetency().toString());
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job testJobFour = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJobFive = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(testJobFour.getId() == testJobFive.getId());
+    }
+
 }
+
+
